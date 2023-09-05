253d9e327d3ace8e2d88921a56ba530f75715d35
Kanyiso Madlebe <TestEmail@mail.com>
2023-09-05 14:09:11 +0200

    Changes to be committed:
    	modified:   src/pages/contact.jsx
    	modified:   src/style/Navbar.css
    	modified:   src/style/contact.css
    	modified:   src/style/home.css

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -43,34 +43,32 @@ function Contact() {
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-  
+
     if (validateForm()) {
       const formData = {
-        to_name: 'Recipient Name', // Replace with the recipient's name
-        from_name: 'Your Name',    // Replace with your name
+        to_name: 'Recipient Name',
+        from_name: 'Your Name',
         name: name,
         email: email,
         phone: phone,
         message: message,
       };
-  
+
       emailjs
         .sendForm('service_418kf8u', 'template_f1av4bc', event.target, 'f4Du8qRs57A9YQRWG')
         .then(
           (result) => {
-            console.log('EmailJS Success:', result.text);
-            console.log('Message sent');
-  
+            console.log('Email sent successfully:', result.text);
             // Clear the form fields after successful submission
             setName('');
             setEmail('');
             setPhone('');
             setMessage('');
-  
+
             // Show the confirmation message
             setShowConfirmation(true);
-  
-            // Hide the confirmation message after a few seconds (optional)
+
+            // Hide the confirmation message after a few seconds
             setTimeout(() => {
               setShowConfirmation(false);
             }, 5000); // Adjust the time as needed
@@ -82,8 +80,7 @@ function Contact() {
         );
     }
   };
-  
-        
+
   const isValidEmail = (email) => {
     // Basic email validation (you can use a library for more robust validation)
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -95,48 +92,63 @@ function Contact() {
       <div className="contact" id="contact">
         <form onSubmit={handleFormSubmit}>
           <h3>Having any queries? Contact Me:</h3>
-          <input
-            type="text"
-            id="user_name"
-            placeholder="Full Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-            autoComplete="off" 
-          />
-          <span className="error">{nameError}</span>
-          <input
-            type="email"
-            id="user_email"
-            placeholder="name@mail.com"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            autoComplete="off" 
-          />
-          <span className="error">{emailError}</span>
-          <input
-            type="number"
-            id="user_phone"
-            placeholder="Phone number"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            required
-            autoComplete="off" 
-          />
-          <span className="error">{phoneError}</span>
-          <textarea
-            id="message"
-            rows="4"
-            placeholder="How can we help you?"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          ></textarea>
+          <div className="field">
+            <input
+              type="text"
+              id="user_name"
+              name="user_name"
+              placeholder="Full Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              autoComplete="off"
+            />
+            <span className="error">{nameError}</span>
+          </div>
+          <div className="field">
+            <input
+              type="email"
+              id="user_email"
+              name="user_email"
+              placeholder="name@mail.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              autoComplete="off"
+            />
+            <span className="error">{emailError}</span>
+          </div>
+          <div className="field">
+            <input
+              type="text"
+              id="user_phone"
+              name="user_phone"
+              placeholder="Phone number"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              required
+              autoComplete="off"
+            />
+            <span className="error">{phoneError}</span>
+          </div>
+          <div className="field">
+            <textarea
+              id="message"
+              name="message"
+              rows="4"
+              placeholder="How can we help you?"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+          </div>
+
           <button type="submit">Submit</button>
         </form>
-
         {/* Confirmation message */}
-        <div className="confirmation-message" style={{ display: showConfirmation ? 'block' : 'none' }}>
+        <div
+          className="confirmation-message"
+          style={{ display: showConfirmation ? 'block' : 'none' }}
+        >
           Your message has been sent successfully!
         </div>
       </div>
