dbfd89f5531fdf4da561c03d4630d6bbd51ee313
Kanyiso Madlebe <TestEmail@mail.com>
2023-10-10 11:14:04 +0200

    Changes to be committed: 	deleted:    my-video-background-app/.eslintrc.cjs 	deleted:    my-video-background-app/.gitignore 	deleted:    my-video-background-app/README.md 	deleted:    my-video-background-app/index.html 	deleted:    my-video-background-app/package-lock.json 	deleted:    my-video-background-app/package.json 	deleted:    my-video-background-app/public/vite.svg 	deleted:    my-video-background-app/src/App.css 	deleted:    my-video-background-app/src/App.jsx 	deleted:    my-video-background-app/src/assets/react.svg 	deleted:    my-video-background-app/src/index.css 	deleted:    my-video-background-app/src/main.jsx 	deleted:    my-video-background-app/vite.config.js 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './pages/Navbar';
-import Home from "./pages/Home"
+import Home from "./pages/home"
 import About from './pages/about';
 import Projects from './pages/projects';
 import Contact from './pages/contact';
